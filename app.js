261c3254ef5b27386f66b5ca7558e00260347c68
melanchorilla-portfolio <dickykamaludin13@gmail.com>
2023-03-28 00:18:00 +0700

    contact app with express pug

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require('express')
 const {body, validationResult, check} = require('express-validator');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
-const {loadContact, findContact, addContact, cekDuplikat, deleteContact, updateContacts} = require('./utils/contacts');
+const flash = require('connect-flash')
+const { loadContact, findContact, addContact, duplicateCheck, deleteContact, updateContact } = require('./utils/contacts')
 
 const app = express()
 const port = 3000
 
-// gunakan ejs
-app.set('view engine', 'ejs')
+// pug settings
+app.set('view engine', 'pug')
+app.set('views', './views')
 
-// Third party middleware
-app.use(expressLayouts)
-
-// built-in middleware
+// built in middleware
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
@@ -31,145 +28,146 @@ app.use(
 );
 app.use(flash());
 
+
 app.get('/', (req, res) => {
     res.render('index', {
-        layout: 'layouts/main-layout',
-        nama: 'Dicky Kamaludin Bashar',
-        title: 'Halaman Home',
+        name: 'Dicky Kamaludin Bashar', 
+        title: 'Home page',
+        isIndex: true
     })
 })
+
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        layout: 'layouts/main-layout',
-        title: "Halaman About",
+        title: 'About page',
+        isAbout: true,
+        
     })
 })
 
-app.get('/contact', (req, res) => {
-    const contacts = loadContact()
 
+app.get('/contact', (req, res) => {
     res.render('contact', {
-        layout: 'layouts/main-layout',
-        title: "Halaman Contact",
-        contacts: contacts,
+        title: 'Contact page',
+        isContact: true,
+        contacts: loadContact(),
         msg: req.flash('msg')
     })
 })
 
+
+// form add contact
 app.get('/contact/add', (req, res) => {
     res.render('add-contact', {
-        layout: 'layouts/main-layout',
-        title: 'Form Tambah Data Contact'
+        title: "Form Contact Data",
+        isContact: true,
     })
 })
 
-app.get('/contact/:nama', (req, res) => {
-    const contact = findContact(req.params.nama);
-    
-    res.render('detail', {
-        layout: 'layouts/main-layout',
-        title: "Halaman Detail Contact",
-        contact: contact
-    });
-});
 
-// proses data contact
-app.post('/contact',
-    [
-        body('nama').custom((value) => {
-            const duplikat = cekDuplikat(value);
-            if(duplikat) {
-                throw new Error('Nama contact sudah digunakan!');
-            }
-            return true;
-        }),
-        check('email', 'E-mail tidak valid!').isEmail(),
-        check('noHP', 'No. HP tidak valid!').isMobilePhone('id-ID')
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            // return respose.status(400).json({errors: errors.array()});
-            res.render('add-contact', {
-                title: 'Form Tambah Data Contact',
-                layout: 'layouts/main-layout',
-                errors: errors.array()
-            })
+// proses add contact
+app.post('/contact', [
+    body('nama').custom((value) => {
+        const duplikat = duplicateCheck(value);
+        if(duplikat) {
+            throw new Error('Contact name is being used!');
         }
-        else {
-            addContact(req.body);
-            // kirimkan flash message
-            req.flash('msg', 'Data contact berhasil ditambahkan!');
-            res.redirect('/contact');
-        }
-});
+        return true;
+    }),
+    check('email', 'Invalid E-mail').isEmail(),
+    check('noHP', 'Invalid Nomor HP').isMobilePhone('id-ID')
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        // return res.status(400).json({errors: errors.array()})
+        res.render('add-contact', {
+            title: "Form Contact Data",
+            isContact: true,
+            errors: errors.array()
+        })
+    } else {
+        addContact(req.body)
+        // kirim flash message
+        req.flash('msg', 'Data has been added!')
+        res.redirect('/contact')
+    }
+})
 
 
-// form ubah data contact
+// form edit data contact
 app.get('/contact/edit/:nama', (req, res) => {
-    const contact = findContact(req.params.nama);
+    const contact = findContact(req.params.nama)
 
     res.render('edit-contact', {
-        layout: 'layouts/main-layout',
-        title: 'Form Ubah Data Contact',
+        title: 'Edit Form Contact Data',
+        isContact: true,
         contact: contact
-    });
-});
+    })
+})
+
 
 // proses ubah data contact
-app.post('/contact/update',
-    [
-        body('nama').custom((value, {req}) => {
-            const duplikat = cekDuplikat(value);
-            if(value !== req.body.oldNama && duplikat) {
-                throw new Error('Nama contact sudah digunakan!');
-            }
-            return true;
-        }),
-        check('email', 'E-mail tidak valid!').isEmail(),
-        check('noHP', 'No. HP tidak valid!').isMobilePhone('id-ID')
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            res.render('edit-contact', {
-                title: 'Form Ubah Data Contact',
-                layout: 'layouts/main-layout',
-                errors: errors.array(),
-                contact: req.body
-            })
+app.post('/contact/update', [
+    body('nama').custom((value, {req}) => {
+        const duplicate = duplicateCheck(value)
+
+        if(value !== req.body.oldNama && duplicate) {
+            throw new Error('Contact name is being used')
         }
-        else {
-            updateContacts(req.body);
-            // kirimkan flash message
-            req.flash('msg', 'Data contact berhasil diubah!');
-            res.redirect('/contact');
-        }
-});
+        return true
+    }),
+    check('email', 'Invalid E-mail!').isEmail(),
+    check('noHP', 'Invalid No. HP').isMobilePhone('id-ID')
+], (req, res) => {
+    const errors =validationResult(req)
+
+    if (!errors.isEmpty()) {
+        res.render('edit-contact', {
+            title: 'Edit Form Contact Data',
+            isContact: true,
+            errors: errors.array(),
+            contact: req.body
+        })
+    } else {
+         updateContact(req.body)
+        // kirimkan flash message
+        req.flash('msg', 'Contact data has been updated!')
+        res.redirect('/contact')
+    }
+})
+
 
 // proses delete contact
 app.get('/contact/delete/:nama', (req, res) => {
-    const contact = findContact(req.params.nama);
+    const contact = findContact(req.params.nama)
     
     // jika contact tidak ada
     if(!contact) {
         res.status(404);
-        res.send('<h1>404</h1>');
+        res.send('<h1>404</h1>')
     } else {
         deleteContact(req.params.nama);
-        req.flash('msg', 'Data contact berhasil dihapus!');
-        res.redirect('/contact');
+        req.flash('msg', 'Data has been deleted!')
+        res.redirect('/contact')
     }
 });
 
 
-app.use('/', (req, res) => {
-    res.status(404);
-    res.send('<h1>404</h1>')
-});
+app.get('/contact/:nama', (req, res) => {
+    res.render('detail', {
+        title: 'Detail Contact',
+        isContact: true,
+        contact: findContact(req.params.nama)
+    })
+})
 
+
+app.use('/', (req, res) => {
+    res.status(404)
+    res.send(`<h1>404</h1>`)
+})
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+    console.log(`App listening on http://localhost:${port}`)
 })
--- utils/contacts.js
+++ utils/contacts.js
@@ -1,50 +1,49 @@
-const fs = require('fs');
+const fs = require('fs')
 
 // membuat folder jika tidak ada
-const dirPath = './data';
-if(!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
+const dirPath = './data'
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
 }
 
 // membuat file contacts.json jika tidak ada
-const dataPath='./data/contacts.json';
-if(!fs.existsSync(dataPath)) {
-    fs.writeFileSync(dataPath, '[]', 'utf-8');
+const dataPath = './data/contacts.json'
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
-// membaca isi semua data contact.json
+// membaca isi semua data contacts.json
 const loadContact = () => {
-    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf8');
-    const contacts = JSON.parse(fileBuffer);
-    return contacts;
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8')
+    const contacts = JSON.parse(fileBuffer)
+
+    return contacts
+}
+
+// menuliskan / menimpa file contacts.json dengan data baru
+const saveContact = (contacts) => {
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
+}
+
+// menambahkan data contact baru
+const addContact = (contact) => {
+    const contacts = loadContact()
+    contacts.push(contact)
+    saveContact(contacts)
+}
+
+// cek nama yang duplikat
+const duplicateCheck = (nama) => {
+    const contacts = loadContact()
+    return contacts.find(contact => contact.nama == nama)
 }
 
 // ambil data berdasarkan nama
 const findContact = (nama) => {
-    const contacts = loadContact();
-
-    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
-
-    return contact;
-}
-
-// menuliskan / menimpa file contacts.json dengan data yang baru
-const saveContacts = (contacts) => {
-    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-}
-
-
-// menambahkan data contact baru
-const addContact = (contact) => {
-    const contacts = loadContact();
-    contacts.push(contact);
-    saveContacts(contacts);
-}
-
-// cek nama yang duplikat
-const cekDuplikat = (nama) => {
-    const contacts = loadContact();
-    return contacts.find((contact) => contact.nama == nama);
+    const contacts = loadContact()
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase())
+    
+    return contact
 }
 
 // hapus contact
@@ -52,19 +51,20 @@ const deleteContact = (nama) => {
     const contacts = loadContact();
     const filteredContacts = contacts.filter((contact) => contact.nama !== nama);
 
-    saveContacts(filteredContacts);
+    saveContact(filteredContacts);
 }
 
-// mengubah contacts
-const updateContacts = (contactBaru) => {
+// mengubah contact
+const updateContact = (newContact) => {
     const contacts = loadContact();
     // hilangkan contact lama yang namanya sama dengan oldNama
-    const filteredContacts = contacts.filter((contact) => contact.nama !== contactBaru.oldNama);
+    const filteredContacts = contacts.filter((contact) => contact.nama !== newContact.oldNama);
 
-    delete contactBaru.oldNama;
+    delete newContact.oldNama;
 
-    filteredContacts.push(contactBaru);
-    saveContacts(filteredContacts);
+    filteredContacts.push(newContact);
+    saveContact(filteredContacts);
 }
 
-module.exports = { loadContact, findContact, addContact, cekDuplikat, deleteContact, updateContacts };
+
+module.exports = { loadContact, findContact, addContact, duplicateCheck, deleteContact, updateContact }
